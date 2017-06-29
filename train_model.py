998d22aa5f28c430db6668fe3ae7bdcf95f12623
dcarnino <deniscarnino@gmail.com>
2017-06-29 10:21:23 +0200

    add: check shapes and lengths for task 40

--- train_model.py
+++ train_model.py
@@ -294,7 +294,7 @@ def train_for_each_task(df_labels_train, df_labels_val, target_size=(299,299),
     ### loop over tasks
     for tid in different_tasks:
 
-        if tid > 30:
+        if tid > 39:
 
             if verbose >= 1: print("Training for task %d..."%tid)
 
@@ -304,6 +304,10 @@ def train_for_each_task(df_labels_train, df_labels_val, target_size=(299,299),
             le.fit(df_task_train.labelId)
             different_classes = le.classes_
             n_classes = len(different_classes)
+
+            print("different_classes: ", different_classes)
+            print("n_classes: ", n_classes)
+            print("len df task train: ", len(df_task_train))
 
             ### Store LabelEncoder
             with gzip.open(model_dir+'label_encoder_%d.pkl'%tid, 'wb') as iOF:
@@ -340,6 +344,13 @@ def train_for_each_task(df_labels_train, df_labels_val, target_size=(299,299),
             y_train = np.array(y_train)
             X_val = np.array(X_val)
             y_val = np.array(y_val)
+
+            print grouped_df_train.shape
+            print grouped_df_val.shape
+            print(X_train.shape)
+            print(y_train.shape)
+            print(X_val.shape)
+            print(y_val.shape)
 
             ### Train model
             if verbose >= 1: print("\tFine-tuning Inception V3 first two passes (task %d)..."%tid)
